3878375ed8f532fa53dd96633b33b99a1e73d10d
kemmily Souza <kemmilyps1@gmail.com>
2023-07-14 12:01:48 -0300

    OrderStatus enum

--- src/main/java/com/cursoudemy/course/entities/Order.java
+++ src/main/java/com/cursoudemy/course/entities/Order.java
@@ -3,8 +3,11 @@ package com.cursoudemy.course.entities;
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
+
+import com.cursoudemy.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+import org.hibernate.query.sql.internal.ParameterRecognizerImpl;
 
 @Entity
 @Table(name = "tb_order")
@@ -16,6 +19,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
 
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -26,10 +31,11 @@ public class Order implements Serializable {
     }
 
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         super();
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -47,6 +53,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
