81454a36b30333e4f8b3195d8660f32382c019a8
kemmily Souza <kemmilyps1@gmail.com>
2023-07-14 13:36:32 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/cursoudemy/course/entities/Product.java
+++ src/main/java/com/cursoudemy/course/entities/Product.java
@@ -21,7 +21,10 @@ public class Product implements Serializable {
     private String imgUrl;
 
     //user Set e n list para que o produto só tenha uma categoria
-    @Transient
+    @ManyToMany
+    @JoinTable(name = "tb_product_category",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
     public Product() {
