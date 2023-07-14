d402a0a1aec6dce1fd2c755e0124f7144556e521
kemmily Souza <kemmilyps1@gmail.com>
2023-07-14 15:21:30 -0300

    OrderItem, many-to-many association with extra atributes

--- src/main/java/com/cursoudemy/course/entities/Order.java
+++ src/main/java/com/cursoudemy/course/entities/Order.java
@@ -2,7 +2,9 @@ package com.cursoudemy.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.cursoudemy.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -25,6 +27,9 @@ public class Order implements Serializable {
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
 
     public Order() {
@@ -71,6 +76,10 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
     }
 
     @Override
